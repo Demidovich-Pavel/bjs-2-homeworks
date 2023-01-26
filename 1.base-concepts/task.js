f6c1a8b7fd93b86ac6fe7e0cbc5d207e8cf4b1b2
Pavel-Demidovich <pdemidovich16@gmail.com>
2023-01-26 10:38:00 +0700

    Квадратное уровнение готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,14 +2,19 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let d = b ** 2 - 4 * a * c;
-  if (d > 0) {
-    x1 = -b + Math.sqrt(d) / (2 * a);
-    x2 = -b - Math.sqrt(d) / (2 * a);
-    arr.push(x1, x2);
-  } else if (d === 0) {
-    x = -b / (2 * a);
-    arr.push(x);
+  if (a !== 0) {
+    if (d > 0) {
+      x1 = (-b + Math.sqrt(d)) / (2 * a);
+      x2 = (-b - Math.sqrt(d)) / (2 * a);
+      arr.push(x1, x2);
+    } else if (d === 0) {
+      x = -b / (2 * a);
+      arr.push(x);
+    };
+  } else {
+    console.log('На ноль делить НЕЛЬЗЯ!');
   };
+
   return arr;
 }
 solveEquation(1, 5, 4);
