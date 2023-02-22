466153098b66e308a7ea74a4dc14310d86babdfd
Pavel-Demidovich <pdemidovich16@gmail.com>
2023-02-22 21:19:28 +0700

    Первое задание, первая часть

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -10,10 +10,10 @@ class PrintEditionItem {
         return this.state = this.state * 1, 5;
     }
     set currentState(newState) {
-        if (this.state < 0) {
+        if (this.newState < 0) {
             this.state = 0;
         }
-        else if (this.state > 100) {
+        else if (this.newState > 100) {
             this.state = 100;
         } else {
             this.state = newState;
