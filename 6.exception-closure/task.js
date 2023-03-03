5f0c947bac892f4793f969806204b4e5170b77c2
Pavel-Demidovich <pdemidovich16@gmail.com>
2023-03-03 23:09:50 +0700

    Initial commit

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,15 @@
-﻿
+﻿function parseCount(parsArg) {
+    let parseCountVar = Number.parseFloat(parsArg);
+    try {
+        if (isNaN(parseCountVar)) {
+            throw new Error('error');
+        }
+        console.log(parseCountVar);
+        return Number.parseFloat(parsArg)
+    }
+    catch (error) {
+        console.log('Невалидное значение')
+    }
+}
+
+function validateCount()
