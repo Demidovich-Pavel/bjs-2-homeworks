8f16b9a2b7d3973202d3547f15d705fa3ebb8848
Pavel-Demidovich <pdemidovich16@gmail.com>
2023-03-15 21:39:22 +0700

    задача 2/2/2 new

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -7,15 +7,6 @@
     return Number.parseFloat(parsArg)
 }
 
-//function parseCount (parsArg){
-//    let parseCountVar = Number.parseFloat(parsArg);
-//    if (isNaN(parseCountVar)) {
-//        throw new Error ('Невалидное значений');
-//    }
-//    console.log(parseCountVar);
-//    return Number.parseFloat(parsArg)
-//  }
-// Пробовал написать два задания в одном. Не получилось))) Скажите, тактая реализация может быть? Не в рамках ДЗ, а впринципи?
 
 
 function validateCount(parsArg) {
