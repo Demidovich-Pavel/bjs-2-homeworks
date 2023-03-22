b8e90c07abbaa6adaabe642a07d928b800cb2c49
Pavel-Demidovich <pdemidovich16@gmail.com>
2023-03-22 23:29:35 +0700

    initial commit 2

--- 7.async/task.js
+++ 7.async/task.js
@@ -10,9 +10,9 @@ class AlarmClock {
         for (let i = 0; a < this.alarmCollection.length; i++) {
             if (this.alarmCollection[i].time === time) {
                 console.warn('Уже присутствует звонок на это же время')
-                let newAlarm = { time, canCall: true, callback };
-                this.alarmCollection.push(newAlarm);
             }
+            let newAlarm = { time, canCall: true, callback };
+            this.alarmCollection.push(newAlarm);
         }
     }
     removeClock(time) {
